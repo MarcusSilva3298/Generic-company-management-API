d6870c124e1dff9dd2c9023d64b02fd398391690
MarcusSilva3298 <marcussilva3298@gmail.com>
2020-04-01 12:05:42 -0300

    Added create function

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -2,10 +2,26 @@ const connection = require('../database/connection');
 const crypto = require('crypto');
 
 module.exports = {
-    //Listagem de todos os produtos
+    //List all products
     async index(require, response){
-        const products = await connection('products').select('*');
+        const products = await connection('inventory').select('*');
 
         return response.json(products);
+    },
+
+    //Create product
+    async create(require, response){
+        const { name, mainCategory, subCategory, amount, 
+            price, cost, sendCompany, arrival
+        } = require.body;
+
+        const productID = crypto.randomBytes(4).toString('HEX');
+
+        await connection('inventory').insert({
+            productID, name, mainCategory, subCategory, amount, 
+            price, cost, sendCompany, arrival,
+        });
+
+        return response.json({ 'Product created! ID': productID })
     }
 }
