92125a814a93a702753391f2f4479ca39b8ba550
MarcusSilva3298 <marcussilva3298@gmail.com>
2020-04-01 12:07:15 -0300

    get and post /inventory methods

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const routes = express.Router();
 
+const ProductsController = require('./controllers/ProductsController');
+routes.route('/inventory')
+    .get(ProductsController.index)
+
+    .post(ProductsController.create)
 
 module.exports = routes;
