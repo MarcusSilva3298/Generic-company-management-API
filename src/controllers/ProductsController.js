7d4be3d96ed63b3d6d51273552e1105cb87c158a
MarcusSilva3298 <marcussilva3298@gmail.com>
2020-04-02 11:35:00 -0300

    Added red, update, delete + index adjustments

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -4,7 +4,16 @@ const crypto = require('crypto');
 module.exports = {
     //List all products
     async index(require, response){
-        const products = await connection('inventory').select('*');
+        const { page = 1 } = require.query;
+        const { filters } = require.params;
+
+        const products = await connection('inventory')
+            .limit(10).offset((page - 1 ) * 10)
+            .select('name', 'mainCategory', 'amount', 'price', 'productID');
+
+        const [ count ] = await connection('inventory').count();
+
+        response.header('X-Total-Products', count['count(*)'])
 
         return response.json(products);
     },
@@ -23,5 +32,57 @@ module.exports = {
         });
 
         return response.json({ 'Product created! ID': productID })
+    },
+
+    //Read one specific product
+    async read(require, response){
+        const { id } = require.params;
+
+        const product = await connection('inventory')
+            .where('productID', id).select('*').first();
+
+        if ( product === undefined ){
+            return response.status(404).json({ error: `ProductID: ${ id } not found!` });
+        }
+        
+        return response.json(product);
+    },
+
+    //Update one product
+    async update(require, response){
+        const { id } = require.params;
+
+        const { name, mainCategory, subCategory, amount, 
+            price, cost, sendCompany, arrival
+        } = require.body;
+
+        const newProduct = await connection('inventory')
+            .where('productID', id).first().update({
+                name, mainCategory, subCategory, amount, 
+                price, cost, sendCompany, arrival,
+            }).select('productID');
+
+        if ( newProduct ===  undefined ){
+            return response.status(404).json({ error: `ProductID: ${ id } not found!` });
+        }
+
+        return response.status(201).json(`Product ${ id } updated!`);
+    },
+
+    //Delete one product
+    async delete(require, response){
+        const { id } = require.params;
+
+        const product = await connection('inventory')
+            .where('productID', id).first().select('productID');
+
+        if ( product === undefined ){
+            return response.status(404).json({ error: `ProductID: ${ id } not found!` });
+        }
+
+        await connection('inventory')
+            .where('productID', id).delete()
+
+        return response.status(204).json(`ProductID: ${id} deleted`);
     }
 }
